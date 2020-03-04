4d791bd3b6d8fcc414ebabcac32e302466057b22
Luke Lombardi <luke@smartshare.io>
2020-03-04 00:06:43 -0500

    removed shit

--- modules/parameters.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def from_config(keys):
-    config = {}
-    for k in keys:
-        config[k] = os.getenv(k, None)
-    return config
--- tests/test_example.py
+++
@@ -1,6 +0,0 @@
-from modules.example import *
-
-
-def test_example():
-    assert returns_string() == "valid_value"
-
